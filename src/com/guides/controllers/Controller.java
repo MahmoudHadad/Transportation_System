086a65e98af2ab5d44488f9adc84312a8db46af3
Ahmed Zaher Abd El-Wahed <ahmedzaher54@gmail.com>
2015-09-09 14:02:08 -0700

    Update Controller.java
    controller bug 2

--- src/com/guides/controllers/Controller.java
+++ src/com/guides/controllers/Controller.java
@@ -2,23 +2,33 @@ package com.guides.controllers;
 
 import java.util.logging.Logger;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.Produces; 
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.mvc.Viewable;
  
+
 
 @Path("/")
 @Produces("text/html")
 public class Controller {
+	
 	private static final Logger logger = Logger.getLogger(Controller.class
 			.getCanonicalName());
 	
 	static{
-		logger.warning("In Controller class");
+		logger.warning("In Services controller");
 	}
+	@Path("/test")
 	@GET
-	@Path("/testController") 
-	public String testController() {
-		return "Test Controller";
+	public Response test(){
+		logger.warning("In Test");
+		return Response.ok(new Viewable("/Test")).build();
+		
 	}
+ 
 }
